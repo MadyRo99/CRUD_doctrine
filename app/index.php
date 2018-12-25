16f6f5af3ef75fd3ce9600935baf366ac1633040
MadyRo99 <madalindanescu99@gmail.com>
2018-12-26 00:42:54 +0200

    Displaying articles on home page + added bootstrap + auth validation.

--- app/index.php
+++ app/index.php
@@ -1,39 +1,95 @@
 <?php
 	session_start();
+	require_once ("../connection.php");
+	$articles = new ArticlesImplement();
+	$articles = $articles->getAllArticles();	
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Home</title>
+	<title>Acasa</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<?php
-		if (isset($_SESSION['id'])) {
-	?>
-	<a href="process/logout_process.php">Log Out</a><br><br>
-	<?php
-	} else { ?>
-	<h1>Log In</h1>
-	<form action="process/login_process.php" method="POST">
-		<input type="text" name="usem" placeholder="Username/Email"><br><br>
-		<input type="password" name="password" placeholder="Password"><br><br>
-		<input type="submit" value="Log In"><br><br>
-	</form>
-	<a href="signup.php">Sign Up</a><br><br>
-	<a href="#">Password Recovery</a>
-	<?php
-		}
-		if (isset($_GET['success'])) {
-			switch ($_GET['success']) {
-				case 0:
-					echo "<br><p style='color: red;'>Contul nu exista in baza de date!</p>";
-					break;
-				case 1:
-					echo "<br><p style='color: red;'>Parola este gresita!</p>";
-					break;
-			}
-		}
-	?>
+<div class="container">
+  	<div class="row">
+    	<div class="col"></div>
+    		<div class="col-8">
+			<?php
+				if (isset($_SESSION['id'])) {
+			?>
+			<a class="btn btn-primary" href="process/logout_process.php">Log Out</a><br><br>
+			<a class="btn btn-primary" href="articole_add.php">Adauga articol</a><br><br>
+			<form class="form-inline" style="width: 100%;">
+			  <div class="form-group" style="width: 90%;">
+			    <input style="width: 99%;" type="text" class="form-control" placeholder="Cauta cuvinte cheie...">
+			  </div>
+			  <button type="submit" class="btn btn-info">Cauta</button>
+			</form>
+			<?php
+			} else { ?>
+			<h1 id="title-spacing">Log In</h1>
+			<form action="process/login_process.php" method="POST">
+				<div class="form-group">
+					<input class="form-control" type="text" name="usem" placeholder="Username / E-mail" required="required"><br><br>
+					<input class="form-control" type="password" name="password" placeholder="Parola" required="required"><br><br>
+					<input class="btn btn-primary" type="submit" value="Log In"><br><br>
+				</div>
+			</form>
+			<a href="signup.php">Sign Up</a><br><br>
+			<a href="#">Recuperare parola</a>
+			<?php
+				}
+				if (isset($_GET['success'])) {
+					switch ($_GET['success']) {
+						case 0:
+							echo "<p class='loginfail' style='color: red;'>Contul nu exista in baza de date!</p>";
+							break;
+						case 1:
+							echo "<p class='loginfail' style='color: red;'>Parola este gresita!</p>";
+							break;
+					}
+				}
+				echo '<br><br>';
+				foreach ($articles as $article) {
+					switch ($article->getCategory()) {
+						case '1':
+							$categorie_article = "Politic";
+							break;
+						case '2':
+							$categorie_article = "IT";
+							break;	
+						case '3':
+							$categorie_article = "Sport";
+							break;
+						case '4':
+							$categorie_article = "Monden";
+							break;
+						case '5':
+							$categorie_article = "Stiri";
+							break;
+					}
+					echo "
+						<h3 style='margin-top: 50px;'>".$article->getTitle()."</h3>
+						<p style='color: red; font-weight: 600; text-decoration: underline;'>".$categorie_article."</p>
+						<p style='text-indent: 50px;'>".$article->getText()."</p>
+						<p style='font-weight: 650;'><span style='color: red;'>Tags: </span>".$article->getTags()."</p>";
+						if (isset($_SESSION['id']) && $_SESSION['id'] == $article->getUidUser())
+						echo "
+						<a href='articole_edit' style='float: left; margin-right: 10px;' class='btn btn-info'>Editeaza</a>
+						<form action='process/articole_delete_process.php' method='GET'>
+							<input class='btn btn-danger' type='submit' name='delete' value='Sterge'>
+							<input type='hidden' name='uid_article' value=".$article->getIdArticles().">
+						</form>
+						<div style='float: both'></div>
+					";
+				}
+			?>
+    		</div>
+    	<div class="col"></div>
+  	</div>
+</div>
 </body>
 </html>
--- app/signup.php
+++ app/signup.php
@@ -7,39 +7,51 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Sign Up</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	
-	<a href="index.php">Home</a>
-	<h1>Sign Up</h1>
-	<form action="process/signup_process.php" method="POST">
-		<input id="name" type="text" name="name" placeholder="Name"><br>
-		<p id="name_info" style="color: red;"></p>
-		<input id="username" type="text" name="username" placeholder="Username"><br>
-		<p id="username_info" style="color: red;"></p>
-		<input id="email" type="email" name="email" placeholder="Email"><br>
-		<p id="email_info" style="color: red;"></p>
-		<input id="password" type="password" name="password" placeholder="Password"><br>
-		<p id="password_info" style="color: red;"></p>
-		<input type="submit" name="signup" value="Sign Up">
-	</form>
-	<?php
-		if (!isset($_GET['result'])) {
-			if (isset($_GET['success']) && $_GET['success']) {
-				echo '<h2>Inregistrarea a avut loc cu succes!</h2>';
-			}
-		} else {
-			$result = json_decode($_GET['result']);
-			if (isset($_GET['success']) && $_GET['success'] == 'false') {
-				foreach ($result as $key => $value) {
-					foreach ($value as $i => $l) {
-						echo '<p>'.$l.'</p>';	
+<div class="container">
+  	<div class="row">
+	    <div class="col"></div>
+			<div class="col-8">
+			<a href="index.php">< Acasa</a>
+			<h1 id="title-spacing">Sign Up</h1>
+			<form action="process/signup_process.php" method="POST">
+				<div class="form-group">
+					<input class="form-control" id="name" type="text" name="name" placeholder="Nume" required="required"><br>
+					<p class="error_p" id="name_info" style="color: red;"></p>
+					<input class="form-control" id="username" type="text" name="username" placeholder="Username" required="required"><br>
+					<p class="error_p" id="username_info" style="color: red;"></p>
+					<input class="form-control" id="email" type="email" name="email" placeholder="E-mail" required="required"><br>
+					<p class="error_p" id="email_info" style="color: red;"></p>
+					<input class="form-control" id="password" type="password" name="password" placeholder="Parola" required="required"><br>
+					<p class="error_p" id="password_info" style="color: red;"></p>
+					<input class="btn btn-primary" type="submit" name="signup" value="Sign Up"><br><br>
+				</div>
+			</form>
+			<?php
+				if (!isset($_GET['result'])) {
+					if (isset($_GET['success']) && $_GET['success']) {
+						echo '<div class="alert alert-success" role="alert">Inregistrarea a avut loc cu succes!</div>';
+					}
+				} else {
+					$result = json_decode($_GET['result']);
+					if (isset($_GET['success']) && $_GET['success'] == 'false') {
+						echo '<div class="alert alert-danger" role="alert">';
+						foreach ($result as $key => $value) {
+							foreach ($value as $i => $l) {
+								echo $l.'<br>';
+							}
+						}
+						echo '</div>';
 					}
 				}
-			}
-		}
-	?>
-
+			?>
+			</div>
+		<div class="col"></div>
+	</div>
+</div>
 <script
   src="https://code.jquery.com/jquery-3.3.1.js"
   integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
@@ -56,10 +68,16 @@ $(document).ready(function(){
 	$('#name').focusout(function () {
 		var name = $('#name').val();
 		if (name === "") {
+			$('#name').removeClass("is-valid");
+			$('#name').addClass("is-invalid");
 			$('#name_info').text("Numele trebuie completat!");
 		} else if (!/^[a-zA-Z]+$/.test(name)) {
+			$('#name').removeClass("is-valid");
+			$('#name').addClass("is-invalid");
 			$('#name_info').text("Numele nu este valid!");
 		} else {
+			$('#name').removeClass("is-invalid");
+			$('#name').addClass("is-valid");
 			$('#name_info').text("");
 		}
 	});
@@ -70,10 +88,16 @@ $(document).ready(function(){
 	$('#username').focusout(function () {
 		var username = $('#username').val();
 		if (username === "") {
+			$('#username').removeClass("is-valid");
+			$('#username').addClass("is-invalid");
 			$('#username_info').text("Username-ul trebuie completat!");
 		} else if (username.length < 6) {
+			$('#username').removeClass("is-valid");
+			$('#username').addClass("is-invalid");
 			$('#username_info').text("Username-ul este prea scurt!");
 		} else {
+			$('#username').removeClass("is-invalid");
+			$('#username').addClass("is-valid");
 			$('#username_info').text("");
 		}
 	});
@@ -84,10 +108,16 @@ $(document).ready(function(){
 	$('#email').focusout(function () {
 		var email = $('#email').val();
 		if (email === "") {
+			$('#email').removeClass("is-valid");
+			$('#email').addClass("is-invalid");
 			$('#email_info').text("Email-ul trebuie completat!");
 		} else if (!validateEmail(email)) {
+			$('#email').removeClass("is-valid");
+			$('#email').addClass("is-invalid");
 			$('#email_info').text("Email-ul nu este valid!");
 		} else {
+			$('#email').removeClass("is-invalid");
+			$('#email').addClass("is-valid");
 			$('#email_info').text("");
 		}
 	});
@@ -98,10 +128,16 @@ $(document).ready(function(){
 	$('#password').focusout(function () {
 		var password = $('#password').val();
 		if (password === "") {
+			$('#password').removeClass("is-valid");
+			$('#password').addClass("is-invalid");
 			$('#password_info').text("Parola trebuie completata!");
 		} else if (password.length < 6) {
+			$('#password').removeClass("is-valid");
+			$('#password').addClass("is-invalid");
 			$('#password_info').text("Parola este prea scurta!");
 		} else {
+			$('#password').removeClass("is-invalid");
+			$('#password').addClass("is-valid");
 			$('#password_info').text("");
 		}
 	});
